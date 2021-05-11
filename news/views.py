e4f04c1a8946f2651cf46e6369b36f4eb5b182df
Alicia-krynne <alicemacrineadhiambo@gmail.com>
2021-05-11 23:59:34 +0300

    update templates

--- news/views.py
+++ news/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from django.http  import HttpResponse,Http404
+from .models import Article
 import datetime as dt
 
 # Create your views here.
@@ -9,7 +10,8 @@ import datetime as dt
 
 def news_of_day(request):
     date = dt.date.today()
-    return render(request, 'all-news/today-news.html', {"date": date,})
+    news = Article.todays_news()
+    return render(request, 'all-news/today-news.html', {"date": date,"news":news})
 
 def convert_dates(dates):
 
@@ -37,5 +39,6 @@ def past_days_news(request, past_date):
     if date == dt.date.today():
         return redirect(news_of_day)
 
-    return render(request, 'all-news/past-news.html', {"date": date})
+    news = Article.days_news(date)
+    return render(request, 'all-news/past-news.html',{"date": date,"news":news})
 
