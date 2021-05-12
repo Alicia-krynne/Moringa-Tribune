17a6da663ef0a62c3ff05160f2be6fa5aafd43bc
Alicia-krynne <alicemacrineadhiambo@gmail.com>
2021-05-12 15:43:15 +0300

    displaying  image in article

--- news/urls.py
+++ news/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns=[
@@ -7,3 +9,5 @@ urlpatterns=[
     path('archives/<str:past_date>/',views.past_days_news,name='pastNews'),
     path('search/', views.search_results, name='search_results'),
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
