751c62314e37dda868682833c0d4127f0dd12436
Alicia-krynne <alicemacrineadhiambo@gmail.com>
2021-05-17 22:24:08 +0300

    add news letter

--- news/views.py
+++ news/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render,redirect
-from django.http  import HttpResponse,Http404
-from .models import Article
+from django.http  import HttpResponse,Http404,HttpResponseRedirect
+from .models import Article,NewsLetterRecipients
+from .forms import NewsLetterForm
 import datetime as dt
+from .email import send_welcome_email
 
 # Create your views here.
 
@@ -9,9 +11,20 @@ import datetime as dt
     #return render(request, 'welcome.html')
 
 def news_today(request):
-    date = dt.date.today()
-    news = Article.todays_news()
-    return render(request, 'all-news/today-news.html', {"date": date,"news":news})
+    date= dt.date.today()
+    news= Article.todays_news()
+    if request.method == 'POST':
+        form = NewsLetterForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['your_name']
+            email = form.cleaned_data['email']
+            recipient = NewsLetterRecipients(name = name,email =email)
+            recipient.save()
+            send_welcome_email(name,email)
+    else:
+            form = NewsLetterForm()
+
+    return render(request, 'all-news/today-news.html', {"date": date,"news":news,"letterForm":form})
 
 def convert_dates(dates):
 
